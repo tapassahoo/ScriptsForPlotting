f4eb6d8713a27399836b28624f1002028e264f19
Dr. Tapas Sahoo, Ph.D <tapascuchem@gmail.com>
2022-08-09 21:58:26 +0530

    Darwin: Updated on 09-08-2022

--- get-figure-rotational-propagator.py
+++ get-figure-rotational-propagator.py
@@ -1,12 +1,32 @@
+#!/usr/bin/env python
+""" 
+This code is exploited for a 3-dimensional surface plot. 
+"""
+
+__author__ = "Dr. Tapas Sahoo"
+__contact__ = "tapascuchem@gmail.com"
+__copyright__ = "Copyright 2022, GITHUB"
+__credits__ = ["Dr. Tapas Sahoo"]
+__date__ = "2022/08/06"
+__deprecated__ = False
+__email__ =  "tapascuchem@gmail.com"
+__license__ = "GPLv3"
+__maintainer__ = "Dr. Tapas Sahoo"
+__status__ = "Production"
+__version__ = "0.0.1"
+
+# Import modules
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import os
 
 # Reading the file, we would like to plot.
-inputFile="/home/tapassahoo/academic-project/outputs/rotational-propagator-for-water/rho.den018_ortho"
-rho = np.genfromtxt(inputFile, unpack=True, usecols=[3])
-print(max(rho))
+home=os.path.expanduser("~")
+inputFile=home+"/academic-project/outputs/rotational-propagator-for-water/rho.den018_ortho"
+print(inputFile)
 exit()
+rho = np.genfromtxt(inputFile, unpack=True, usecols=[3])
 
 size_grid = 361
 theta=np.arange(361)
