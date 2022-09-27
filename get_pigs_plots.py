fc407dff25e03267535dbbe08e49474c13ec2b64
Tapas Sahoo <tapascuchem@gmail.com>
2022-09-27 17:15:17 +0530

    Linux: Updated on 27-09-2022

--- get_pigs_plots.py
+++ get_pigs_plots.py
@@ -16,7 +16,7 @@ rotational_move = True
 
 molecular_system = "HF"
 rotor = "HF"
-numb_molecule = 4
+numb_molecule = 2
 
 parameter_name = "beta"
 parameter_value = 0.1
@@ -33,7 +33,7 @@ if (parameter_name == "tau"):
 if (parameter_name == "beta"):
 	variable_name = "tau"
 
-distance_flag = True
+distance_flag = False
 plot_type = "energy"
 
 if (method == "PIGS"):
@@ -44,7 +44,7 @@ elif (method == "PIMC"):
 final_result_path = os.path.expanduser(
 	"~") + "/academic-project/output/final-" + file_name_modifier + "-outputs-for-plotting/"
 
-rlist = np.arange(3.0, 10.01, 0.2, dtype=float)
+rlist = np.arange(3.0, 10.01, 1.0, dtype=float)
 
 if (((rotational_move) and (translational_move == False) and (plot_type ==
 															  "energy")) and ((parameter_name == "tau") or (parameter_name == "beta"))):
